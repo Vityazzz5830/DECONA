9a4d32686b28cccd16228bd30174e2bfd2cfc862
Vityazzz5830 <121909224+Vityazzz5830@users.noreply.github.com>
2024-10-27 20:40:21 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,34 @@
+$('.catalogsec_mainpage_content_tabmenu_sliderone').slick({
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: false,
+    infinite: false,
+    dots: false,
+    autoplay: false,
+    variableWidth: true
+});
+
+
+$('.blogsec_mainpage_slider').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: false,
+    infinite: false,
+    dots: false,
+    autoplay: false,
+    variableWidth: true
+});
+
+
+
+
+
+
+
+
+
+
+
+
